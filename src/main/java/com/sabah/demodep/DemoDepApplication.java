0db6e94c10307ff4ff70252bd1ab2d91bfe8553f
Aleksandr Kravtsov <sabahtalateh@gmail.com>
2019-12-07 22:54:04 +0300

    init

---
+++ src/main/java/com/sabah/demodep/DemoDepApplication.java
@@ -0,0 +1,13 @@
+package com.sabah.demodep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sabah/demodep/DemoDepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sabah.demodep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
